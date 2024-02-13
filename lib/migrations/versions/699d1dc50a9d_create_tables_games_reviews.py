ecfd0a5ee0ed02f15ef38e324b67b02758690d01
John-thabuks <thabuksjohn@gmail.com>
2024-02-13 21:29:19 +0300

    This is more confusing that space science

---
+++ lib/migrations/versions/699d1dc50a9d_create_tables_games_reviews.py
@@ -0,0 +1,24 @@
+"""create_tables_games_reviews
+
+Revision ID: 699d1dc50a9d
+Revises: faebd12f9523
+Create Date: 2024-02-13 21:13:51.841876
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '699d1dc50a9d'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
